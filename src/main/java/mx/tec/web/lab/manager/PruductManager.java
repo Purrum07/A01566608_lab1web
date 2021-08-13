c01570156a7c7b500a9c0a4dde875660bf726cbc
Mario Márquez <48485832+Purrum07@users.noreply.github.com>
2021-08-12 19:37:42 -0600

    Challenge 3

--- src/main/java/mx/tec/web/lab/manager/PruductManager.java
+++ src/main/java/mx/tec/web/lab/manager/PruductManager.java
@@ -25,7 +25,7 @@ public class PruductManager {
 	public PruductManager() {
 		products = new ArrayList<>();
 		
-		Product product1 = new Product("prod123", "Levi's 501 Original Fit Jeans Jeans para Hombre", "100% algodón, Cierre de Cremallera, Lavar a máquina, Jeans corte ajustado, Pierna ajustada, Stretch especial que te brinda mayor movilidad", Collections.emptyList());
+		Product product1 = new Product("prod123", "Levi's 501 Original Fit Jeans Jeans para Hombre", "100% algodón, Cierre de Cremallera, Lavar a máquina, Jeans corte ajustado, Pierna ajustada, Stretch especial que te brinda mayor movilidad", "https://via.placeholder.com/150", "https://via.placeholder.com/300", "https://via.placeholder.com/2048", Collections.emptyList());
 		List<Sku> childSkus1 = new ArrayList<>();		
 		childSkus1.add(new Sku("sku1234", "Black", "29W X 32L", 1027.24, 1027.24, 500));
 		childSkus1.add(new Sku("sku1235", "Dark Stonewash", "29W X 32L", 1027.24, 706.93, 200));
@@ -33,7 +33,7 @@ public class PruductManager {
 		
 		products.add(product1);
 		
-		Product product2 = new Product("prod124", "Levi's Innovation Super Skinny Jeans para Mujer", "85% Algodón, 9% Elastomultiester, 6% Elastano, Lavar a máquina, Pantalón, Mezclilla, Cintura Media, Ajustado Desde la Cadera al Muslo, Pierna Súper Ajustada", Collections.emptyList());
+		Product product2 = new Product("prod124", "Levi's Innovation Super Skinny Jeans para Mujer", "85% Algodón, 9% Elastomultiester, 6% Elastano, Lavar a máquina, Pantalón, Mezclilla, Cintura Media, Ajustado Desde la Cadera al Muslo, Pierna Súper Ajustada", "https://via.placeholder.com/50", "https://via.placeholder.com/360", "https://via.placeholder.com/4096", Collections.emptyList());
 		List<Sku> childSkus2 = new ArrayList<>();		
 		childSkus2.add(new Sku("sku1236", "Black Galaxy", "25W X 30L", 661.79, 661.79, 300));
 		childSkus2.add(new Sku("sku1237", "Black Galaxy", "26W X 30L", 661.79, 661.79, 400));
--- src/main/java/mx/tec/web/lab/vo/Product.java
+++ src/main/java/mx/tec/web/lab/vo/Product.java
@@ -6,6 +6,9 @@ public class Product {
 	private String id;
 	private String name;
 	private String description;
+	private String smallUrl;
+	private String mediumUrl;
+	private String largeUrl;
 	private List<Sku> childSkus;
 
 	public Product() {
@@ -15,12 +18,18 @@ public class Product {
 	 * @param id
 	 * @param name
 	 * @param description
+	 * @param smallUrl
+	 * @param mediumUrl
+	 * @param largeUrl
 	 * @param childSkus
 	 */
-	public Product(String id, String name, String description, List<Sku> childSkus) {
+	public Product(String id, String name, String description, String smallUrl, String mediumUrl, String largeUrl, List<Sku> childSkus) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
+		this.smallUrl = smallUrl;
+		this.mediumUrl = mediumUrl;
+		this.largeUrl = largeUrl;
 		this.childSkus = childSkus;
 	}
 	
@@ -60,6 +69,48 @@ public class Product {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	/**
+	 * @return the smallUrl
+	 */
+	public String getSmallUrl() {
+		return smallUrl;
+	}
+
+	/**
+	 * @param smallUrl the smallUrl to set
+	 */
+	public void setSmallUrl(String smallUrl) {
+		this.smallUrl = smallUrl;
+	}
+
+	/**
+	 * @return the mediumUrl
+	 */
+	public String getMediumUrl() {
+		return mediumUrl;
+	}
+
+	/**
+	 * @param mediumUrl the mediumUrl to set
+	 */
+	public void setMediumUrl(String mediumUrl) {
+		this.mediumUrl = mediumUrl;
+	}
+
+	/**
+	 * @return the largeUrl
+	 */
+	public String getLargeUrl() {
+		return largeUrl;
+	}
+
+	/**
+	 * @param largeUrl the largeUrl to set
+	 */
+	public void setLargeUrl(String largeUrl) {
+		this.largeUrl = largeUrl;
+	}
+
 	/**
 	 * @return the childSkus
 	 */
